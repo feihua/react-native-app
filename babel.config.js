6ea337c74d19caf93227deaf8630cab3a1913bd6
koobe <koobe.liu@uaf.com.cn>
2024-03-21 17:50:21 +0800

    配置图片目录别名@images方便使用

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,17 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: ["module:metro-react-native-babel-preset"],
+  plugins: [
+    [
+      "module-resolver",
+      {
+        root: ["./src"],
+        extensions: [".ios.js", ".android.js", ".js", ".ts", ".tsx", ".json", ".png", ".jpg"],
+        alias: {
+          "@": "./src/",
+          "@views": "./src/views/",
+          "@images": "./src/assets/images/",
+        },
+      },
+    ],
+  ],
 };
